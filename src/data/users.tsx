bf8e2af3d51581f682f53acd4d67bcf52e5b27c0
Lucas Moura <lucasgabrieldemoura.2021@gmail.com>
2023-03-06 13:55:21 -0300

    data

--- src/data/users.tsx
+++ src/data/users.tsx
@@ -7,32 +7,32 @@ export default [
     },
     {
         id: 2,
-        name: "Lucas Gabriel 2",
-        email: "lucasg2@gmail.com",
+        name: "Jurandir Silva",
+        email: "jsilva@gmail.com",
         avatarUrl: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
     },
     {
         id: 3,
-        name: "Lucas Gabriel 3",
-        email: "lucasg3@gmail.com",
+        name: "Lua Prigan",
+        email: "lua@gmail.com",
         avatarUrl: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
     },
     {
         id: 4,
-        name: "Lucas Gabriel 4",
-        email: "lucasg4@gmail.com",
+        name: "Petrov Jorska",
+        email: "petroj@gmail.com",
         avatarUrl: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
     },
     {
         id: 5,
-        name: "Lucas Gabriel 5",
-        email: "lucasg5@gmail.com",
+        name: "José Alencar",
+        email: "jalencar@gmail.com",
         avatarUrl: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
     },
     {
         id: 6,
-        name: "Lucas Gabriel 6",
-        email: "lucasg6@gmail.com",
+        name: "Michael Morales",
+        email: "mMorales@gmail.com",
         avatarUrl: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
     },
 
